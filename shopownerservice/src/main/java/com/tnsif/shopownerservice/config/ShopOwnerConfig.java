dfc87b5cd5be951070eb8b9b0303e653b97c2a76
Asus <Asus@192.168.1.33>
2025-11-08 08:58:28 +0530

    updated

--- shopownerservice/src/main/java/com/tnsif/shopownerservice/config/ShopOwnerConfig.java
+++ shopownerservice/src/main/java/com/tnsif/shopownerservice/config/ShopOwnerConfig.java
@@ -1,16 +1,27 @@
 package com.tnsif.shopownerservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
-@EnableWebMvc
+@CrossOrigin(origins = "http://localhost:3000")  // React connection
 public class ShopOwnerConfig {
 	
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000/")
-			.allowedMethods("GET", "POST", "PUT", "DELETE");
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
 	}
+	
 }
